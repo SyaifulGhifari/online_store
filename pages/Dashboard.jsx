5ea40527a965be9c758ca733e48bdca44bc1ea5e
zainal547 <zainal547@gmail.com>
2022-08-01 09:48:48 +0700

    mengerjakan tampilan dashboard Saat tidak login

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,4 +1,5 @@
-import Image from "next/dist/client/image";
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 export default function Dashboard() {
   return (
     <div className="bg-white py-6 sm:py-8 lg:py-12">
@@ -10,6 +11,47 @@ export default function Dashboard() {
             Ini adalah website Scarva tempat di mana anda bisa melihat dan memilih sepatu yang anda sukai jadi silahkan pilh dan kami akan mengantarkan kepada anda.
           </p>
         </div>
+
+        {/* colom product */}
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {/* product start */}
+          <div>
+            <a href="#" className="group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100">
+              <Image src="/Nike-shoes.jpg" width="700" height="700" alt="Photo by unsplash" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+            </a>
+            <div className="flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4">
+              <div className="flex flex-col">
+                <a href="#" className="font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg">
+                  Nike Air Force 1 NDESTRUKT
+                </a>
+              </div>
+              <div className="flex flex-col items-end">
+                <span className="font-bold text-black lg:text-lg mt-7">$15.00</span>
+              </div>
+            </div>
+          </div>
+          {/* product end */}
+
+          {/* product start */}
+          <div>
+            <a href="#" className="group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100">
+              <Image src="/Nike-shoes.jpg" width="700" height="700" alt="Photo by unsplash" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+            </a>
+
+            <div className="flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4">
+              <div className="flex flex-col">
+                <a href="#" className="font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg">
+                  Nike Air Force 1 NDESTRUKT
+                </a>
+              </div>
+              <div className="flex flex-col items-end">
+                <span className="font-bold text-black lg:text-lg mt-7">$15.00</span>
+              </div>
+            </div>
+          </div>
+          {/* product end */}
+        </div>
+        {/* end colom product */}
       </div>
     </div>
   );
