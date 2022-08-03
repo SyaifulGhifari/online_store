f10bb98fdec0cb76a2fd4f1d1d07c11af6c2c515
syaifulghifari <syaiful.ghifari12@gmail.com>
2022-08-03 10:59:10 +0700

    get product

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,58 +1,95 @@
+import { useState, useEffect } from 'react';
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+
+import NavbarUser from '../components/NavbarUser';
+
 export default function Dashboard() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log('FETFCHH DATA');
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    console.log('FETFCHH DATA');
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/products`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response);
+      const data = await response.json();
+      if (response.status === 200) {
+        console.log('Success:', data);
+      } else if (response.status >= 300) {
+        throw data.message;
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div className="mb-10 md:mb-16">
-          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Selamat Datang</h2>
+    <>
+      <NavbarUser />
+      <div className='bg-white py-6 sm:py-8 lg:py-12'>
+        <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+          <div className='mb-10 md:mb-16'>
+            <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>
+              Selamat Datang
+            </h2>
 
-          <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
-            Ini adalah website Scarva tempat di mana anda bisa melihat dan memilih fashion di sini menyediakan Hodie, baju dan kemeja yang anda sukai jadi silahkan pilh dan kami akan menyimpannya untuk anda.
-          </p>
-        </div>
+            <p className='mx-auto max-w-screen-md text-center text-gray-500 md:text-lg'>
+              Ini adalah website Scarva tempat di mana anda bisa melihat dan
+              memilih fashion di sini menyediakan Hodie, baju dan kemeja yang
+              anda sukai jadi silahkan pilh dan kami akan menyimpannya untuk
+              anda.
+            </p>
+          </div>
 
-        {/* colom product */}
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {/* product start */}
-          <div>
-            <a href="#" className="group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100">
-              <Image src="/Nike-shoes.jpg" width="700" height="700" alt="Photo by unsplash" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-            </a>
-            <div className="flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4">
-              <div className="flex flex-col">
-                <a href="#" className="font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg">
-                  Nike Air Force 1 NDESTRUKT
+          {/* colom product */}
+          <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {/* product start */}
+            <div>
+              <Link href='#'>
+                <a className='group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100'>
+                  <Image
+                    src='/Nike-shoes.jpg'
+                    width='700'
+                    height='700'
+                    alt='Photo by unsplash'
+                    className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                  />
                 </a>
-              </div>
-              <div className="flex flex-col items-end">
-                <span className="font-bold text-black lg:text-lg mt-7">$15.00</span>
+              </Link>
+              <div className='flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4'>
+                <div className='flex flex-col'>
+                  <Link href='#'>
+                    <a className='font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg'>
+                      Nike Air Force 1 NDESTRUKT
+                    </a>
+                  </Link>
+                </div>
+                <div className='flex flex-col items-end'>
+                  <span className='font-bold text-black lg:text-lg mt-7'>
+                    $15.00
+                  </span>
+                </div>
               </div>
             </div>
+            {/* product end */}
           </div>
-          {/* product end */}
-
-          {/* product start */}
-          <div>
-            <a href="#" className="group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100">
-              <Image src="/Nike-shoes.jpg" width="700" height="700" alt="Photo by unsplash" className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-            </a>
-
-            <div className="flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4">
-              <div className="flex flex-col">
-                <a href="#" className="font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg">
-                  Nike Air Force 1 NDESTRUKT
-                </a>
-              </div>
-              <div className="flex flex-col items-end">
-                <span className="font-bold text-black lg:text-lg mt-7">$15.00</span>
-              </div>
-            </div>
-          </div>
-          {/* product end */}
+          {/* end colom product */}
         </div>
-        {/* end colom product */}
       </div>
-    </div>
+    </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,11 @@
-//import Navbar from "../components/Navbar";
-//import Productdetail from "./ProductDetail";
-import NavbarUser from "../components/NavbarUser";
-import Historyorder from "./Historyorder";
-//import Cart from "./Cart";
-import Profileuser from "./Profileuser";
-
-//import Dashboard from "./Dashboard";
+import Dashboard from './Dashboard';
 
 //export async function getServerSideProps() {}
 
 export default function Home() {
   return (
     <div>
-      <NavbarUser />
-      {/* <Cart /> */}
-      {/* <Dashboard /> */}
-      {/* <Productdetail /> */}
-      <Profileuser />
-      {/* <Historyorder /> */}
+      <Dashboard />
     </div>
   );
 }
