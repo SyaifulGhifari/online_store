ef1f5cd5d31faf30740e4e0bed87635b00aed0af
zainal547 <zainal547@gmail.com>
2022-08-05 08:19:05 +0700

    Mengubah nama file pada upername

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
-import NavbarUser from '../components/NavbarUser';
-import Navbar from '../components/Navbar';
+import Image from "next/image";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import NavbarUser from "../components/navbarUser";
+import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
   const [products, setProducts] = useState([]);
@@ -16,15 +16,12 @@ export default function Dashboard() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/products`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/products`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       const data = await response.json();
       if (response.status === 200) {
         setProducts(data.data);
@@ -33,61 +30,48 @@ export default function Dashboard() {
         throw data.message;
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <>
       {isLogin ? <NavbarUser /> : <Navbar />}
-      <div className='bg-white py-6 sm:py-8 lg:py-12'>
-        <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
-          <div className='mb-10 md:mb-16'>
-            <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>
-              Selamat Datang
-            </h2>
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-10 md:mb-16">
+            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Selamat Datang</h2>
 
-            <p className='mx-auto max-w-screen-md text-center text-gray-500 md:text-lg'>
-              Ini adalah website Scarva tempat di mana anda bisa melihat dan
-              memilih fashion di sini menyediakan Hodie, baju dan kemeja yang
-              anda sukai jadi silahkan pilh dan kami akan menyimpannya untuk
-              anda.
+            <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+              Ini adalah website Scarva tempat di mana anda bisa melihat dan memilih fashion di sini menyediakan Hodie, baju dan kemeja yang anda sukai jadi silahkan pilh dan kami akan menyimpannya untuk anda.
             </p>
           </div>
 
           {/* colom product */}
-          <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {/* product start */}
             {products.map((product) => {
               return (
                 <div key={product.id}>
                   <Link href={`/product/${product.id}`}>
-                    <a className='group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100'>
+                    <a className="group relative block h-80 overflow-hidden rounded-t-lg bg-gray-100">
                       <Image
-                        src={
-                          product.product_image == ''
-                            ? '/bajuputih.jpg'
-                            : product.product_image
-                        }
-                        width='700'
-                        height='700'
-                        alt='Photo by unsplash'
-                        className='h-full w-full object-cover object-center transition duration-200 group-hover:scale-110'
+                        src={product.product_image == "" ? "/bajuputih.jpg" : product.product_image}
+                        width="700"
+                        height="700"
+                        alt="Photo by unsplash"
+                        className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                       />
                     </a>
                   </Link>
-                  <div className='flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4'>
-                    <div className='flex flex-col'>
+                  <div className="flex items-start justify-between gap-2 rounded-b-lg bg-gray-100 p-4">
+                    <div className="flex flex-col">
                       <Link href={`/product/${product.id}`}>
-                        <a className='font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg'>
-                          {product.product_name}
-                        </a>
+                        <a className="font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-lg">{product.product_name}</a>
                       </Link>
                     </div>
-                    <div className='flex flex-col items-end'>
-                      <span className='font-bold text-black lg:text-lg mt-7'>
-                        ${product.price}
-                      </span>
+                    <div className="flex flex-col items-end">
+                      <span className="font-bold text-black lg:text-lg mt-7">${product.price}</span>
                     </div>
                   </div>
                 </div>
--- pages/Profileuser.jsx
+++ pages/Profileuser.jsx
@@ -1,19 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import Image from 'next/image';
-import NavbarUser from '../components/NavbarUser';
-import { setLogout } from '../redux/auth';
-import Swal from 'sweetalert2';
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import NavbarUser from "../components/navbarUser";
+import { setLogout } from "../redux/auth";
+import Swal from "sweetalert2";
 
 export default function Profileuser() {
   const [dataUser, setDataUser] = useState({
-    Name: '',
-    Address: '',
-    Phone: '',
-    Email: '',
-    Password: '',
+    Name: "",
+    Address: "",
+    Phone: "",
+    Email: "",
+    Password: "",
   });
   const isLogin = useSelector((state) => state.auth.isLogin);
   const token = useSelector((state) => state.auth.token);
@@ -21,8 +21,8 @@ export default function Profileuser() {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  if (isLogin === '') {
-    router.replace('/');
+  if (isLogin === "") {
+    router.replace("/");
   }
 
   const handleChangeName = (e) => {
@@ -41,16 +41,13 @@ export default function Profileuser() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
+        method: "GET",
+        headers: {
+          Authorization: `bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
       const data = await response.json();
       if (response.status === 200) {
         setDataUser(data.data);
@@ -59,74 +56,68 @@ export default function Profileuser() {
         throw data.message;
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const handleSubmitSave = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
-        {
-          method: 'PUT',
-          headers: {
-            Authorization: `bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            name: dataUser.Name,
-            address: dataUser.Address,
-            phone: dataUser.Phone,
-          }),
-        }
-      );
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
+        method: "PUT",
+        headers: {
+          Authorization: `bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: dataUser.Name,
+          address: dataUser.Address,
+          phone: dataUser.Phone,
+        }),
+      });
       const data = await response.json();
       if (response.status === 200) {
-        console.log('Success:', data);
+        console.log("Success:", data);
       } else if (response.status >= 300) {
         throw data.message;
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   const handleDelete = async () => {
     Swal.fire({
-      title: 'Do you want to delete your account?',
+      title: "Do you want to delete your account?",
       showDenyButton: true,
       showCancelButton: true,
-      confirmButtonText: 'yess!',
+      confirmButtonText: "yess!",
       denyButtonText: `NO!`,
     }).then(async (result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
         try {
-          const response = await fetch(
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}/users`,
-            {
-              method: 'DELETE',
-              headers: {
-                Authorization: `bearer ${token}`,
-                'Content-Type': 'application/json',
-              },
-            }
-          );
+          const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
+            method: "DELETE",
+            headers: {
+              Authorization: `bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          });
           const data = await response.json();
           if (response.status === 200) {
-            console.log('Success:', data);
+            console.log("Success:", data);
             dispatch(setLogout());
-            Swal.fire('Deleted!', '', 'success');
-            router.push('/');
+            Swal.fire("Deleted!", "", "success");
+            router.push("/");
           } else if (response.status >= 300) {
             throw data.message;
           }
         } catch (error) {
-          console.error('Error:', error);
+          console.error("Error:", error);
         }
       } else if (result.isDenied) {
-        Swal.fire('canceled', '', 'info');
+        Swal.fire("canceled", "", "info");
       }
     });
   };
@@ -134,87 +125,48 @@ export default function Profileuser() {
   return (
     <>
       <NavbarUser />
-      <div className='bg-white py-6 sm:py-8 lg:py-12'>
-        <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
+      <div className="bg-white py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
           <div>
-            <input
-              type='file'
-              className='w-1/4 items-center rounded border border-black'
-            />
-            <div className='grid gap-8 md:grid-cols-2'>
-              <Image
-                src='/bajuputih.jpg'
-                width='200'
-                height='500'
-                alt='Photo by Himanshu Dewangan'
-                className='w-1/2 object-cover'
-              />
+            <input type="file" className="w-1/4 items-center rounded border border-black" />
+            <div className="grid gap-8 md:grid-cols-2">
+              <Image src="/bajuputih.jpg" width="200" height="500" alt="Photo by Himanshu Dewangan" className="w-1/2 object-cover" />
 
               {/* content - start */}
-              <div className='md:py-8'>
-                <form onSubmit={handleSubmitSave} className=' flex flex-col'>
-                  <div className='mb-4'>
-                    <label className='mb-1 block'>Name:</label>
-                    <input
-                      onChange={handleChangeName}
-                      value={dataUser.Name}
-                      type='text'
-                      className='form-input h-8 w-1/2 rounded border border-gray-400 '
-                      placeholder='Name'
-                    />
+              <div className="md:py-8">
+                <form onSubmit={handleSubmitSave} className=" flex flex-col">
+                  <div className="mb-4">
+                    <label className="mb-1 block">Name:</label>
+                    <input onChange={handleChangeName} value={dataUser.Name} type="text" className="form-input h-8 w-1/2 rounded border border-gray-400 " placeholder="Name" />
                   </div>
-                  <div className='mb-4'>
-                    <label className='mb-1 block'>Address:</label>
-                    <input
-                      onChange={handleChangeAddress}
-                      value={dataUser.Address}
-                      type='text'
-                      className='form-input h-8 w-1/2 rounded border border-gray-400'
-                      placeholder='Address'
-                    />
+                  <div className="mb-4">
+                    <label className="mb-1 block">Address:</label>
+                    <input onChange={handleChangeAddress} value={dataUser.Address} type="text" className="form-input h-8 w-1/2 rounded border border-gray-400" placeholder="Address" />
                   </div>
-                  <div className='mb-4'>
-                    <label className='mb-1 block'>Phone:</label>
-                    <input
-                      onChange={handleChangeNumber}
-                      value={dataUser.Phone}
-                      type='number'
-                      className='form-input h-8 w-1/2 rounded border border-gray-400'
-                      placeholder='Phone'
-                    />
+                  <div className="mb-4">
+                    <label className="mb-1 block">Phone:</label>
+                    <input onChange={handleChangeNumber} value={dataUser.Phone} type="number" className="form-input h-8 w-1/2 rounded border border-gray-400" placeholder="Phone" />
                   </div>
-                  <div className='mt-16'>
-                    <button
-                      type='submit'
-                      className='rounded-lg bg-city px-5 py-3 text-center text-sm font-semibold text-black hover:bg-city/60 focus-visible:ring active:text-gray-700'
-                    >
+                  <div className="mt-16">
+                    <button type="submit" className="rounded-lg bg-city px-5 py-3 text-center text-sm font-semibold text-black hover:bg-city/60 focus-visible:ring active:text-gray-700">
                       Save
                     </button>
-                    <button
-                      onClick={handleDelete}
-                      className='rounded-lg bg-red-500 mx-3 px-5 py-3 text-center text-sm font-semibold text-black hover:bg-red-300 focus-visible:ring active:text-gray-700'
-                    >
+                    <button onClick={handleDelete} className="rounded-lg bg-red-500 mx-3 px-5 py-3 text-center text-sm font-semibold text-black hover:bg-red-300 focus-visible:ring active:text-gray-700">
                       Delete
                     </button>
-                    <button className='mr-50 rounded-lg bg-gray-500 border border-gray-300 px-8 py-3 text-center text-sm font-semibold text-black hover:bg-gray-400 focus-visible:ring active:text-gray-700'>
-                      Cancel
-                    </button>
+                    <button className="mr-50 rounded-lg bg-gray-500 border border-gray-300 px-8 py-3 text-center text-sm font-semibold text-black hover:bg-gray-400 focus-visible:ring active:text-gray-700">Cancel</button>
                   </div>
                 </form>
               </div>
               {/* content - end */}
             </div>
             {/* buttons - start */}
-            <div className='mt-3 flex justify-between'>
-              <Link href='/historyorder'>
-                <a className='absolute mb-3 rounded-lg border border-gray-400 bg-white px-8 py-3 text-center text-sm font-semibold text-black hover:bg-gray-400 focus-visible:ring active:text-gray-700'>
-                  Hisotry Order
-                </a>
+            <div className="mt-3 flex justify-between">
+              <Link href="/historyorder">
+                <a className="absolute mb-3 rounded-lg border border-gray-400 bg-white px-8 py-3 text-center text-sm font-semibold text-black hover:bg-gray-400 focus-visible:ring active:text-gray-700">Hisotry Order</a>
               </Link>
-              <Link href='/'>
-                <a className='relative mb-3 mt-16 rounded-lg bg-red-600 px-8 py-3 text-center text-sm font-semibold text-black hover:bg-red-500 focus-visible:ring active:text-gray-700'>
-                  Cancel
-                </a>
+              <Link href="/">
+                <a className="relative mb-3 mt-16 rounded-lg bg-red-600 px-8 py-3 text-center text-sm font-semibold text-black hover:bg-red-500 focus-visible:ring active:text-gray-700">Cancel</a>
               </Link>
             </div>
             {/* buttons - end */}
